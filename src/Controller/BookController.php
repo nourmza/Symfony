76b419c1d270fe96aa612254dd112511a284df95
nour mzahi <115937797+nourmza@users.noreply.github.com>
2023-11-10 23:54:03 +0100

    third commit

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -25,11 +25,25 @@ class BookController extends AbstractController
             'controller_name' => 'BookController',
         ]);
     }
+
     #[Route('/Affiche', name: 'app_AfficheBook')]
     public function Affiche(BookRepository $repository)
     {
-        $book = $repository->findAll(); //select*
-        return $this->render('book/affiche.html.twig', ['book' => $book]);
+        //recuperer les livres publié
+        $publishedBooks = $this->getDoctrine()->getRepository(book::class)->findBy(['published' => true]);
+        //compter les nbre des livres publiés et non publiés
+        $numPublishedBooks = count($publishedBooks);
+        $numUnpublishedBooks = count($this->getDoctrine()->getRepository(Book::class)->findBy(['published=false']));
+        // hedhi mte3 lcondition question3
+        if ($numUnpublishedBooks > 0) {
+            return $this->render('book/affiche.html.twig', ['publishedBooks' => $publishedBooks=>$numUnpublishedBooks,'$numUnpublishedBooks'=>$numPublishedBooks]);
+
+        } else {
+            //afficher un message si aucun livre n' a ete trouvé
+            return $this->render('book/no_books_found.html.twig');
+        }
+
+
     }
 
     #[Route('/AddBook', name: 'app_AddBook')]
@@ -43,10 +57,10 @@ class BookController extends AbstractController
             //initialisation de l'attribut "published" a true
             $book->setPublished(true);
             //get the association author from the book entity
-            $author =$book->getAuthorId();
+            $author = $book->getAuthorId();
             //incrementataion de l'attribut "nb_books" de l'entite "Author"
-            if($author instanceof Author){
-                $author->setNbBooks($author->getNbBooks()+1);
+            if ($author instanceof Author) {
+                $author->setNbBooks($author->getNbBooks() + 1);
             }
             $em = $this->getDoctrine()->getManager();
             $em->persist($book);
@@ -57,4 +71,43 @@ class BookController extends AbstractController
 
 
     }
+
+    #[Route('/edit/{ref}', name: 'app_editBook')]
+    function edit(BookRepository $repository, $ref, Request $request)
+    {
+        $author = $repository->find($ref);
+        $form = $this->createForm(BookType::class, $book);
+        $form->add(child: 'Edit', type: SubmitType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute("app_AfficheBook");
+
+        }
+        return $this->render('book/edit.html.twig', ['f' => $form->createView()]);
+    }
+
+    #[Route('/delete/{ref}', name: 'app_deleteBook')]
+    function delete(BookRepository $repository, $id, Request $request)
+    {
+        $book = $repository->find($ref);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($book);
+        $em->flush();
+
+        return $this->redirectToRoute('app_AfficheBook');
+
+    }
+
+    public function showBook($ref, BookRepository $repository)
+    {
+        $book = $repository->find($ref);
+        if (!$book) {
+
+            return $this->redirectToRoute('app_AfficheBook');
+        }
+        return $this->render('book/show.html.twig', ['b' => $book])
+
+}
 }
